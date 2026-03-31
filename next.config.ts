a5d771d2ad97efe02cb2445b7633bf6c573789fe
dpayumo <denapayumo@gmail.com>
2026-03-30 22:43:38 -0400

    setup for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import path from "node:path";
 
 import type { NextConfig } from "next";
 
+const repoName = "is117-dcp2-museum-exhibit";
+const isGithubPagesBuild = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: isGithubPagesBuild ? `/${repoName}` : "",
+  assetPrefix: isGithubPagesBuild ? `/${repoName}/` : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
